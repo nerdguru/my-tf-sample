7f60ee5272d38dfec52f64f5f68242ed18a36af3
Pete Johnson <petecj2@gmail.com>
2021-01-15 15:19:21 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_vpc" "sample_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "sample_vpc"
+  }
+}
+
+resource "aws_subnet" "sample_subnet" {
+  vpc_id     = aws_vpc.sample_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "sample_subnet"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.sample_vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.sample_vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = aws_subnet.sample_subnet.id
+  tags = {
+    Name = "example"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "ami" {
+  default = "ami-08d70e59c07c61a3a"
+  //default = "ami-830c94e3"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
